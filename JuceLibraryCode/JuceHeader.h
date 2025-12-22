e2bee9a882402acd23ff83854136948201cb4946
panyang <pandapan@aactechnologies.com>
2025-12-22 13:40:52 +0800

    update juce version

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -21,6 +21,7 @@
 #include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_audio_plugin_client/juce_audio_plugin_client.h>
 #include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_processors_headless/juce_audio_processors_headless.h>
 #include <juce_audio_utils/juce_audio_utils.h>
 #include <juce_box2d/juce_box2d.h>
 #include <juce_core/juce_core.h>
